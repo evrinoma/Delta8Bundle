136bb72c16eec23129135daa845a5da82c64b442
evrinoma <evrinoma@gmail.com>
2020-10-26 12:06:47 +0300

    little fix menu
    Signed-off-by: evrinoma <evrinoma@gmail.com>

--- src/Menu/Delta8Menu.php
+++ src/Menu/Delta8Menu.php
@@ -7,7 +7,7 @@ namespace Evrinoma\Delta8Bundle\Menu;
 use Doctrine\ORM\EntityManagerInterface;
 use Evrinoma\Delta8Bundle\Voter\Delta8RoleInterface;
 use Evrinoma\MenuBundle\Entity\MenuItem;
-use Evrinoma\MenuBundle\Manager\MenuInterface;
+use Evrinoma\MenuBundle\Menu\MenuInterface;
 
 /**
  * Class Delta8Menu
@@ -17,13 +17,14 @@ use Evrinoma\MenuBundle\Manager\MenuInterface;
 final class Delta8Menu implements MenuInterface
 {
 
-    public function createMenu(EntityManagerInterface $em): void
+    public function create(EntityManagerInterface $em): void
     {
         $journal = new MenuItem();
         $journal
             ->setRole([Delta8RoleInterface::ROLE_USER_DELTA8])
             ->setName('Journal')
-            ->setRoute('delta_journal');
+            ->setRoute('delta_journal')
+            ->setTag($this->tag());
 
         $em->persist($journal);
 
@@ -33,7 +34,7 @@ final class Delta8Menu implements MenuInterface
             ->setName('Delta8')
             ->setUri('#')
             ->addChild($journal)
-        ;
+            ->setTag($this->tag());
 
         $em->persist($menuDelta);
     }
@@ -41,5 +42,10 @@ final class Delta8Menu implements MenuInterface
     public function order(): int
     {
         return 5;
+    }
+
+    public function tag(): string
+    {
+        return MenuInterface::DEFAULT_TAG;
     }
 }
